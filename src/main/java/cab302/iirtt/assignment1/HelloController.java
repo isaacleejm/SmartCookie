31b60a69137f7c27c66c90e38dadfea56b47f5ff
Isaac Lee <n12135682@qut.edu.au>
2025-03-21 11:20:17 +1000

    first commit

---
+++ src/main/java/cab302/iirtt/assignment1/HelloController.java
@@ -0,0 +1,14 @@
+package cab302.iirtt.assignment1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module cab302.iirtt.assignment1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens cab302.iirtt.assignment1 to javafx.fxml;
+    exports cab302.iirtt.assignment1;
+}
