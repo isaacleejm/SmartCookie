6e0a1cc78a02498ba23f3ef77d8f991096f94e00
Isaac Lee <n12135682@qut.edu.au>
2025-04-10 14:10:27 +1000

    IUser Interface Implementation

---
+++ src/main/java/cab302/iirtt/assignment1/IUser.java
@@ -0,0 +1,34 @@
+package cab302.iirtt.assignment1;
+
+public interface IUser {
+    // Allows the User to sign in to an existing account, using an email and password
+    // This method will have two parameters (String email, String Password)
+    // and compare them with very record of Users in the database until a match is found.
+    // If a match is found, the user is brought to the Dashboard Page, and it will be logged in as the matched user;
+    // Otherwise, display error message on the Login Page.
+    abstract void userLogin(String email, String password);
+
+    // Creates a new record of User that will be added to the User database
+    // This method will take in multiple parameters required for user login such as: (name, email, password, confirmPassword, phoneNumber, etc).
+    // Each parameter will have to be tested for correct formatting, such as password has to be n length, and passwords have to be same, or phone number is invalid.
+    // If an incorrect format is used, an error message will display for that error, otherwise, the account will be created successfully,
+    // the User object will be added to the User database, and they will be directed to the login page.
+    abstract void userRegistration(String name, String password, String confirmPassword, String phoneNumber);
+
+    // Allows the User to Logout of an account
+    // This method will have zero parameters ()
+    // The user's current session will end, and they will be directed to the login page.
+    abstract void userLogout();
+
+    // Deletes an existing record of a User from the User Database
+    // All records related to this User (including Fortune, StudyMaterial, StudyGoal) will be deleted along with the User.
+    // Once all data has been deleted, they will be directed to the login page.
+    abstract boolean deleteUser();
+
+    // Modify/Updates the User's information, such as: (name. password, phoneNumber, etc.)
+    // Takes in multiple parameters
+    // If the all parameters are in the correct formatting, the user's data will be updated with the new data and a "successful pop up will display";
+    // otherwise, it will display an error message for that error.
+    //    abstract boolean modifyUser();
+
+}
