af711de218c45200d1d2ece86cc555096ffafecb
Isaac Lee <n12135682@qut.edu.au>
2025-04-13 23:23:45 +1000

    Added Constants.java into .gitignore

--- src/main/java/cab302/iirtt/assignment1/Constants.java
+++ src/main/java/cab302/iirtt/assignment1/Constants.java
@@ -3,5 +3,5 @@ package cab302.iirtt.assignment1;
 // Make your own Gemini API Key from the link below:
 // https://aistudio.google.com/prompts/new_chat?model=gemini-2.0-flash
 public final class Constants {
-    public static final String API_KEY = "Replace with your Gemini API Key HERE";
+    public static final String API_KEY = "REPLACE with your Gemini API Key HERE";
 }
