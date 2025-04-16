0d17bdef18fb0c95007aea4d83484b19fe469cce
Isaac Lee <n12135682@qut.edu.au>
2025-04-16 13:55:09 +1000

    Implemented a way to prevent the sharing of API Keys using .gitignore

--- src/main/java/cab302/iirtt/assignment1/Constants.java
+++
@@ -1,7 +0,0 @@
-package cab302.iirtt.assignment1;
-
-// Make your own Gemini API Key from the link below:
-// https://aistudio.google.com/prompts/new_chat?model=gemini-2.0-flash
-public final class Constants {
-    public static final String API_KEY = "Replace with your Gemini API Key HERE";
-}
