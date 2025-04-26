9d65e659fe6f3025e62f12b315f3e8452eefd31b
Tao Ming Lim <taoming.lim@connect.qut.edu.au>
2025-04-27 06:48:36 +1000

    Added FXML file and Controller for Landing Page, Added Dashboard Controller

--- src/main/java/cab302/iirtt/assignment1/HelloApplication.java
+++ src/main/java/cab302/iirtt/assignment1/HelloApplication.java
@@ -4,17 +4,19 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.paint.Color;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 700);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Dashboard.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         stage.setTitle("Hello There!");
         stage.setScene(scene);
         stage.show();
+        scene.setFill(Color.web("#f8f8f8"));
     }
 
     public static void main(String[] args) {
