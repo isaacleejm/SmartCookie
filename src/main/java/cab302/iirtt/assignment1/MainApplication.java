cf77d3fb5e496b70ff24698527a8ef85cd32009e
isaacleejm <108120700+isaacleejm@users.noreply.github.com>
2025-04-27 12:25:51 +1000

    Update and rename HelloApplication.java to MainApplication.java

--- src/main/java/cab302/iirtt/assignment1/MainApplication.java
+++ src/main/java/cab302/iirtt/assignment1/MainApplication.java
@@ -11,9 +11,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Dashboard.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("Dashboard.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
-        stage.setTitle("Hello There!");
+        stage.setTitle("Dashboard");
         stage.setScene(scene);
         stage.show();
         scene.setFill(Color.web("#f8f8f8"));
