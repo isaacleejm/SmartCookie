1cfd77374a76427e657a38e0813c118a968d102a
Tom Ryan <thomas.ryanefc27@hotmail.com>
2025-05-07 16:47:40 +1000

    Added javadoc commenting for IUser

--- src/main/java/cab302/iirtt/assignment1/model/IAIResponse.java
+++ src/main/java/cab302/iirtt/assignment1/model/IAIResponse.java
@@ -6,7 +6,7 @@ public interface IAIResponse {
 
     /**
      * Generates AI message based on user input and is one of either a fortune cookie, motivational quote, fun prediction or advice/tips.
-     * @param userInput The study material uploaded by the user.
+     * @param userInput The prompt for the AI.
      */
     abstract void generateResponse(String userInput);
 
@@ -24,17 +24,11 @@ public interface IAIResponse {
      */
     abstract List<AIResponse> viewResponses(String userID);
 
-    // User has the option to save their favourite messages
-    // Each user has their own unique list of favourite messages
-
     /**
      * This function lets the user save responses they like as a favourite.
      * @param responseID Unique identification number for AI generated response.
      */
     abstract void toggleFavouriteResponse(int responseID);
-
-    // This feature allows the user to change the tone of the messages being generated
-    // It takes responseType as its only parameter
 
     /**
      * This function allows the user to alter the tone of the AI response.
